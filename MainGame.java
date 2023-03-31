f542e29ccd1bcdad6921db4c73433b6b68e85603
solarrrrrrrrr <samde742@gmail.com>
2023-03-31 09:15:01 -0400

    added player class, removed test code

--- MainGame.java
+++ MainGame.java
@@ -6,34 +6,12 @@ import java.util.Scanner;
 public class MainGame {
     static String currentRoom;
     static HashMap<String, Room> rooms = new HashMap<>();
-    static ArrayList<Item> inventory = new ArrayList<>();
     public static void main(String[] args) {
-        setup();
-        Scanner sc = new Scanner(System.in);
-        while(true) {
-            Room r = rooms.get(currentRoom);
-            System.out.print(r.d);
-            System.out.print("Which direction would you like to go? ");
-            String[] resp = sc.nextLine().split("[ ]+");
-            for(String i : resp) {
-                if(i.equals("read")) {
-                    Item it = r.getItem("plaque"); //FIXME -- how to find which item to look for
-                    System.out.print(it.d);
-                }
-                else {
-                    switch(i) {
-                        case "n":
-                        case "e":
-                        case "s":
-                        case "w":
-                            currentRoom = r.getExits(i.charAt(0));
-                    }
-                }
-            }
+            setup();
+            Scanner sc = new Scanner(System.in);
+        
             //currentRoom = r.getExits();
         } 
-
-    }
 
     static void setup() {
         currentRoom = "foyer";
---
+++ Player.java
@@ -0,0 +1,31 @@
+
+import java.util.ArrayList;
+public class Player {
+    int lives = 5;
+    int food = 5;
+    final String name;
+    ArrayList<Item> inventory = new ArrayList<>();
+
+
+    Player(String name) {
+        this.name = name;
+    }
+
+    void removeLife() {
+        this.lives--;
+        System.out.printf("You now have %d lives.%n", this.lives);
+    }
+
+    void removeFood() {
+        this.food--;
+        System.out.printf("You now have %d hunger.", this.food);
+    }
+
+    void addItem(Item i) {
+        inventory.add(i);
+        
+    }
+
+
+
+}
