4c36cb89497280b4c5001fe5b49cdacd50c37f2c
tetearie054 <tetearie054@gmail.com>
2023-04-17 08:32:58 -0400

    Co-authored-by: solarrrrrrrrr <solarrrrrrrrr@users.noreply.github.com>

--- Item.java
+++ Item.java
@@ -3,6 +3,12 @@ class Item {
     String n, d;
     boolean hidden;
 
+    /**
+     * gets item
+     * @param n name of item
+     * @param d description of item
+     * @param hidden whether the item is hidden or not
+     */
     Item(String n, String d, boolean hidden) {
         this.n = n;
         this.d = d;
