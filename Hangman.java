3b554d0b691869abe106d3ce9c9df97db828a130
solarrrrrrrrr <samde742@gmail.com>
2023-04-18 22:15:05 -0400

    Revert "Co-authored-by: solarrrrrrrrr <solarrrrrrrrr@users.noreply.github.com>"
    This reverts commit 4c36cb89497280b4c5001fe5b49cdacd50c37f2c.

--- Hangman.java
+++ Hangman.java
@@ -1,35 +1,38 @@
 
+
 import java.util.Scanner;
 import java.util.ArrayList;
+
+
 
 public class Hangman {
     static boolean guessed = false;
     
-
     /**
      * show what the person has guessed
      * @param arr the array containing the hidden word
      */
     void showWord(String[] arr) {
         System.out.println();
-
         // print the array of guessed
         for(String s : arr) {
             System.out.print(s + " ");
         }
     }
+
+
     /**
      * show all letters that have been guessed
      * @param arr the array of guessed letters
      */
     void showGuessedLetters(ArrayList<String> arr) {
         System.out.println();
-
         // print each letter
         for(String l : arr) {
             System.out.print(l + " ");
         }
     }
+
 
     /**
      * check if they guessed the word
@@ -37,60 +40,61 @@ public class Hangman {
      * @param g guesses
      */
     void checkGuessedWord(String a, String[] g) {
+        // make the array of correct guesses a string
         String s = "";
         for(int i = 0; i < g.length; i++) {
             s += g[i];
         }
+        // if they are equal --> win
         if(a.equals(s)) guessed = true;
     }
 
-    Hangman() {
-        Scanner sc = new Scanner(System.in);
-        int lives = 9;
-        String answer ="words";
-        String[] hiddenWord = new String[answer.length()];
-        for(int i = 0 ; i < hiddenWord.length; i++) {
-            hiddenWord[i] = "_";
-        }
-        ArrayList<String> letters = new ArrayList<>();
 
-        while(!guessed) {
-            System.out.printf("%nlives: %d%n", lives);
-            if(lives <= 0) {
-                System.out.printf("%nYOU LOSE!%n");
-                break;
+        Hangman() {
+            Scanner sc = new Scanner(System.in);
+            int lives = 9;
+            String answer ="words";
+            // create hidden word array
+            String[] hiddenWord = new String[answer.length()];
+            for(int i = 0 ; i < hiddenWord.length; i++) {
+                hiddenWord[i] = "_";
             }
-            checkGuessedWord(answer, hiddenWord);
-            
-            
-            showWord(hiddenWord);
-            showGuessedLetters(letters);
-            System.out.print("\nWhat is your guess? ");
-            String guess;
-            
-            guess = sc.next();
-            if(guess.length() > 1) continue;
-            if(letters.contains(guess)) {
-                System.out.println("You already guessed this.");
-                continue;
-            }
-            letters.add(guess);
-            
+            ArrayList<String> letters = new ArrayList<>();
 
-            if(answer.contains(guess)) {
-                for(int i = 0; i < answer.length(); i++) {
-                    if(String.valueOf(answer.charAt(i)).equals(guess)) {
-                        hiddenWord[i] = guess.toLowerCase();
+
+            while(!guessed) {
+                System.out.printf("%nlives: %d%n", lives);
+                // if they have 0 lives, break
+                if(lives <= 0) {
+                    System.out.printf("%nYOU LOSE!%n");
+                    break;
+                }
+                checkGuessedWord(answer, hiddenWord);
+                showWord(hiddenWord);
+                showGuessedLetters(letters);
+                // take guess
+                System.out.print("\nWhat is your guess? ");
+                String guess = sc.next();
+                // if they guess with one letter 
+                if(guess.matches("[A-Za-z]{1}")) continue;
+                // if they guessed before
+                if(letters.contains(guess)) {
+                    System.out.println("You already guessed this.");
+                    continue;
+                }
+                letters.add(guess);
+                
+                // if the guess is in the answer, find index of the letter
+                if(answer.contains(guess)) {
+                    for(int i = 0; i < answer.length(); i++) {
+                        // if index of letter, put in array at that index
+                        if(String.valueOf(answer.charAt(i)).equals(guess)) {
+                            hiddenWord[i] = guess.toLowerCase();
+                        }
+                        else lives--;
                     }
                 }
             }
-            else lives--;
-            
+            if(guessed) System.out.println("YOU WIN!");
         }
-        if(lives > 0) {
-            System.out.println("YOU WIN!");
-            
-        }
-        
     }
-}
